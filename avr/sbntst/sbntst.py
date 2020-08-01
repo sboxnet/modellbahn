e36f8e19fc4a2bb6d07d7b05558f1594430dc5c8
sboxnet <tom2@tom.ist-im-web.de>
2020-08-01 18:11:22 +0200

    sbntst initial

---
+++ avr/sbntst/sbntst.py
@@ -0,0 +1,91 @@
+import sys
+import time
+import re
+import signal
+import shlex
+import io
+import readline
+import os.path
+from datetime import datetime
+import atexit
+import rlcompleter
+import csv
+import inspect
+import traceback
+import argparse
+import threading
+
+import sboxnet
+import usb.core
+import usb.util
+
+def getint(v):
+    if v.startswith("0x"):
+        return int(v, 16)
+    return int(v)
+
+def checkbyte(v, name):
+    try:
+        b = getint(v)
+        if (b >= -128 and b < 256):
+            return (b & 0x00ff)
+        print("ERROR: %s is not a byte." % (name))
+    except:
+        print("ERROR: %s is not an integer." % (name))
+    return None
+
+def checkword(v, name):
+    try:
+        b = getint(v)
+        if (b >= -32768 and b < 65536):
+            return (b & 0x00ffff)
+        print("ERROR: %s is not a 16bit word." % (name))
+    except:
+        print("ERROR: %s is not an integer." % (name))
+    return None
+
+def checkint(v, name):
+    try:
+        return (getint(v) & 0x0ffffffff)
+    except:
+        print("ERROR: %s is not an integer." % (name))
+    return None
+
+def highbyte(v):
+    return ((v >> 8) & 0x00ff)
+
+def lowbyte(v):
+    return (v & 0x00ff)
+
+def makeword(v1, v0):
+    return ((v1 & 0xff) << 8) + (v0 & 0xff)
+def makelong(v3, v2, v1, v0):
+    return ((v3 & 0xff) << 24) + ((v2 & 0xff) << 16) + ((v1 & 0xff) << 8) + (v0 & 0xff)
+
+def makeworda(arr, idx):
+    return makeword(arr[idx+1], arr[idx])
+def makelonga(arr, idx):
+    return makelong(arr[idx+3],arr[idx+2],arr[idx+1],arr[idx])
+def log(o, h, s):
+    if o:
+        print('%s:(%s)(%s)(%s.%s):%s:%s' % (datetime.now(), inspect.stack()[2][1], inspect.stack()[2][2], o.__class__.__name__, inspect.stack()[2][3], h, s))
+    else:
+        print('%s:(%s)(%s)(%s):%s:%s' % (datetime.now(), inspect.stack()[2][1], inspect.stack()[2][2], inspect.stack()[2][3], h, s))
+
+def logInfo(o, s):
+    log(o, 'INFO', s)
+
+def logDebug(o, s):
+    log(o, 'DEBUG', s)
+
+def logError(o, s):
+    log(o, 'ERROR', s)
+
+class Sbntst(object):
+    _prdid = sboxnet.SboxnetUSB.PRODUCTID
+    _seq = 0
+    
+    # Sbntst.__init__
+    def __init__(self, dccmap, debug):
+        print("--- SboxnetTester ---")
+        print("creating SboxnetUSB:...")
