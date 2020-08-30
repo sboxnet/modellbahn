660409d1827970b2b648a59171b86457f16e45be
sboxnet <tom2@tom.ist-im-web.de>
2020-08-30 10:39:41 +0200

    dccgen remove watchdog test for on/off

--- avr/sboxnet/sb3/dccgen.c
+++ avr/sboxnet/sb3/dccgen.c
@@ -96,57 +96,6 @@ struct Eeprom {
 };
 struct Eeprom g_eeprom EEMEM;
 
-/*#ifdef USE_BOOSTER
-
-#define LED_BO_NOTAUS_b   0
-#define LED_CUR_OV_b      1
-#define LED_CUR_SHORT_b   2
-
-#define DCCM_PORT  PORTC
-#define DCCM_IN1_b  0
-#define DCCM_IN2_b  1
-#define DCCM_EN1_b  2
-#define DCCM_EN2_b  3
-
-#define CUR_OV_PORT  PORTC
-#define CUR_OV_b   7
-
-#define DCC_CUTOUT_TEST_PORT  PORTC
-#define DCC_CUTOUT_TEST_b   6
-
-#define TIMER_STARTUP  62 // ~1 s
-
-#define BOOSTER_FLAG_ON_b      0
-#define BOOSTER_FLAG_NOTAUS_b  1
-#define BOOSTER_FLAG_CUR_OV_b  2
-
-#define DCC_WATCHDOG_VAL 4 // 4*16ms = 64ms
-
-#define BOOSTER_DEFAULT_SHORTCUT_INTERVAL  35000 // 70ms
-#define BOOSTER_DEFAULT_SHORTCUT_LIMIT     4300
-
-struct booster {
-    uint8_t        flags;
-    struct timer   timer_startup;
-    struct timer   timer_dcc_watchdog;
-    uint16_t       advals[2];
-    uint16_t       shortcut_cnt;
-    uint16_t       shortcut_limit;
-    uint16_t       shortcut_interval;
-    uint16_t       shortcut_nummax;
-    struct {
-        unsigned write_shortcut_limit:1;
-        unsigned write_shortcut_interval:1;
-    }              eeprom_flags;
-};
-struct booster g_booster = { 0, };
-
-#endif // USE_BOOSTER*/
-
-
-#define DCC_IN_PORT PORTC
-#define DCC_IN_b   4
-
 #define LED_DCC_ON_b      6
 #define LED_DCC_NOTAUS_b  7
 
@@ -788,206 +737,6 @@ void dccgen_init(void) {
     loco_list_init();
 }
 
-
-
-/*#ifdef USE_BOOSTER
-
-#include "dccdec.c"
-static void booster_sensors_shortcut_on(void) {
-    if ((PORTC.INTCTRL & PORT_INT0LVL_gm) == PORT_INT0LVL_OFF_gc) {
-        PORTC.INTFLAGS = Bit(PORT_INT0IF_bp);
-        PORTC.INTCTRL = (PORTC.INTCTRL & ~PORT_INT0LVL_gm) | PORT_INT0LVL_LO_gc;
-    }
-}
-
-static void booster_sensors_shortcut_off(void) {
-    PORTC.INTCTRL = (PORTC.INTCTRL & ~PORT_INT0LVL_gm) | PORT_INT0LVL_OFF_gc;
-}
-
-static void do_dec_parse_packet(void) {
-    timer_set(&g_booster.timer_dcc_watchdog, DCC_WATCHDOG_VAL);
-    
-    if (g_dccdec.buf[0] != 0 // broadcast or reset
-           && g_dccdec.buf[0] != 0xff) { // idle
-        // produce cutout with TCD0.CCC
-        ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-            TCD0.CCC = TCD0.CNT + (28/2);
-        }
-        TCD0.INTFLAGS = Bit(TC0_CCCIF_bp);
-        TCD0.INTCTRLB = (TCD0.INTCTRLB & ~TC0_CCCINTLVL_gm)|TC_CCCINTLVL_LO_gc;
-        g_dccdec.cutout = 1;
-        port_setbit(DCC_CUTOUT_TEST_PORT, DCC_CUTOUT_TEST_b); // cutout test point
-    }
-}
-
-// dcc detector cutout generator
-ISR(TCD0_CCC_vect) {
-    switch (g_dccdec.cutout) {
-        case 1: { // cutout enable
-            port_set(DCCM_PORT, Bit(DCCM_EN1_b)|Bit(DCCM_EN2_b)); // EN* on
-            port_clr(DCCM_PORT, Bit(DCCM_IN1_b)|Bit(DCCM_IN2_b)); // 2 lower MOSFETs on, connect to 0V
-            AWEXC.OUTOVEN = 0;                                    // output override enable off
-            ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-                TCD0.CCC = TCD0.CNT + (432/2);
-            }
-            g_dccdec.cutout = 2;
-            booster_sensors_shortcut_off(); // disable shortcut sensors
-            break;
-        }
-        case 2: { //cutout disable
-            AWEXC.OUTOVEN = Bit(3)|Bit(2)|Bit(1)|Bit(0);
-            port_clr(DCCM_PORT, Bit(DCCM_EN1_b)|Bit(DCCM_EN2_b)|Bit(DCCM_IN1_b)|Bit(DCCM_IN2_b));
-            port_clrbit(DCC_CUTOUT_TEST_PORT, DCC_CUTOUT_TEST_b); // cutout test point
-            if (g_dccdec.state != DEC_STATE_OFF) {
-                g_dccdec.state = DEC_STATE_FIRST;
-            }
-            ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-                TCD0.CCC = TCD0.CNT + (2*58/2);
-            }
-            g_dccdec.cutout = 3;
-            break;
-        };
-        case 3:
-        default: {
-            TCD0.INTCTRLB = (TCD0.INTCTRLB & ~TC0_CCCINTLVL_gm)|TC_CCCINTLVL_OFF_gc;
-            g_dccdec.cutout = 0;
-            if (g_booster.timer_startup.value <= 0) {
-                booster_sensors_shortcut_on(); // enable shortcut sensors after first preamble bit to avoid
-                                               // shortcut detections during powerup after cutout
-            }
-        }
-    }
-}
-
-static void booster_sensors_off(void) {
-    PORTC.INTCTRL = (PORTC.INTCTRL & ~PORT_INT0LVL_gm) | PORT_INT0LVL_OFF_gc;
-    PORTC.INT0MASK = 0; // L6206 Stromüberwachung
-    
-    TCD0.INTCTRLB &= ~TC0_CCBINTLVL_gm;
-    
-    g_booster.shortcut_cnt = 0;
-}
-
-void booster_sensors_init(void) {
-    port_set(LED_PORT, Bit(LED_BO_NOTAUS_b)|Bit(LED_CUR_SHORT_b));
-
-    clrbit(g_booster.flags, BOOSTER_FLAG_CUR_OV_b);
-
-    // L6206 Stromüberwachung
-    PORTC.INT0MASK = Bit(CUR_OV_b);
-    PORTC.INTFLAGS = Bit(PORT_INT0IF_bp); // clear interrupt flags;
-    PORTC.INTCTRL = (PORTC.INTCTRL & ~PORT_INT0LVL_gm) | PORT_INT0LVL_OFF_gc;
-    PORTC.PIN7CTRL = PORT_OPC_PULLUP_gc|PORT_ISC_FALLING_gc;
-    
-    g_booster.shortcut_cnt = 0;
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        TCD0.CCB = TCD0.CNT + g_booster.shortcut_interval;
-        TCD0.INTFLAGS = Bit(TC0_CCBIF_bp);
-        TCD0.INTCTRLB = (TCD0.INTCTRLB & ~TC0_CCBINTLVL_gm) | TC_CCBINTLVL_LO_gc;
-    }
-}
-
-void booster_power_off_all(void) {
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        booster_sensors_off();
-
-        port_clr(DCCM_PORT, Bit(DCCM_EN1_b)|Bit(DCCM_EN2_b)|Bit(DCCM_IN1_b)|Bit(DCCM_IN2_b));
-        AWEXC.OUTOVEN = 0;
-
-        booster_sensors_shortcut_off();
-        PORTC.INTCTRL = (PORTC.INTCTRL & ~PORT_INT1LVL_gm) | PORT_INT1LVL_OFF_gc;
-        PORTC.INTFLAGS = Bit(PORT_INT1IF_bp)|Bit(PORT_INT0IF_bp);
-
-        TCD0.INTCTRLB &= ~TC0_CCBINTLVL_gm;
-
-        timer_set(&g_booster.timer_startup, 0xff);
-        
-        dec_stop();
-    }
-}
-
-void booster_power_on_track(void) {
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        PORTC.INT1MASK = Bit(DCC_IN_b);
-        PORTC.PIN4CTRL = PORT_ISC_BOTHEDGES_gc;
-        PORTC.INTFLAGS = Bit(PORT_INT1IF_bp);
-        PORTC.INTCTRL = (PORTC.INTCTRL & ~PORT_INT1LVL_gm) | PORT_INT1LVL_HI_gc;
-                
-        booster_sensors_init();
-        
-        timer_set(&g_booster.timer_startup, TIMER_STARTUP);
-        timer_set(&g_booster.timer_dcc_watchdog, DCC_WATCHDOG_VAL);
-
-        dec_start();
-        
-        uint8_t v = 0;
-        if (bit_is_set(port_in(DCC_IN_PORT), DCC_IN_b)) {
-            v = Bit(TC0_CMPA_bp)|Bit(TC0_CMPB_bp);
-        }
-        TCC0.CTRLC = v;
-        AWEXC.OUTOVEN = Bit(3)|Bit(2)|Bit(1)|Bit(0);
-    }
-}
-
-static void booster_notaus(void) {
-    booster_power_off_all();
-    setbit(g_booster.flags, BOOSTER_FLAG_NOTAUS_b);
-}
-
-ISR(PORTC_INT0_vect) { // L6206 current
-    port_clrbit(LED_PORT, LED_CUR_SHORT_b);
-    
-    if (g_booster.shortcut_cnt <= g_booster.shortcut_limit) {
-        g_booster.shortcut_cnt++;
-    }
-    if (g_booster.shortcut_cnt > g_booster.shortcut_nummax ) {
-        g_booster.shortcut_nummax = g_booster.shortcut_cnt;
-    }
-}
-
-ISR(TCD0_CCB_vect) { // shortcut detector
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        TCD0.CCB = TCD0.CNT + g_booster.shortcut_interval;
-    }
-    if (g_booster.shortcut_cnt >= g_booster.shortcut_limit) {
-        booster_power_off_all();
-        setbit(g_booster.flags, BOOSTER_FLAG_CUR_OV_b);
-        port_clrbit(LED_PORT, LED_CUR_SHORT_b);
-    } else {
-        port_setbit(LED_PORT, LED_CUR_SHORT_b);
-    }
-    g_booster.shortcut_cnt = 0;
-}
-
-static void booster_init(void) {
-    timer_register(&g_booster.timer_startup, TIMER_RESOLUTION_16MS);
-    timer_register(&g_booster.timer_dcc_watchdog, TIMER_RESOLUTION_16MS);
-    dec_init(EVSYS_CHMUX_PORTC_PIN4_gc);
-    g_booster.flags = 0;
-    g_booster.shortcut_limit = e2prom_get_word(&g_eeprom.booster.shortcut_limit);
-    if (g_booster.shortcut_limit == 0xffff) {
-        g_booster.shortcut_limit = BOOSTER_DEFAULT_SHORTCUT_LIMIT;
-    }
-    g_booster.shortcut_interval = e2prom_get_word(&g_eeprom.booster.shortcut_interval);
-    if (g_booster.shortcut_interval == 0xffff) {
-        g_booster.shortcut_interval = BOOSTER_DEFAULT_SHORTCUT_INTERVAL;
-    }
-    g_booster.shortcut_nummax = 0;
-    g_booster.eeprom_flags.write_shortcut_limit = 0;
-    g_booster.eeprom_flags.write_shortcut_interval = 0;
-    booster_power_off_all();
-}
-
-ISR(PORTC_INT1_vect) { // DCC Input Signal
-    uint8_t v = 0;
-    if (bit_is_set(port_in(DCC_IN_PORT), DCC_IN_b)) {
-        v = Bit(TC0_CMPA_bp)|Bit(TC0_CMPB_bp);
-    }
-    TCC0.CTRLC = v;
-}
-
-#endif // USE_BOOSTER*/
-
 static void dcc_read_switches(void) {
     uint8_t sw;
 
@@ -1000,43 +749,10 @@ static void dcc_read_switches(void) {
 }
 
 
-
-/*#ifdef USE_BOOSTER
-static void measure_task(void) {
-    if (ADCA.INTFLAGS == 0x03) {
-        int16_t ch0;
-        int16_t ch1;
-        // channel 0 and 1 conversion finished
-        ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-            ch0 = (int16_t)ADCA.CH0.RES;
-            ch1 = (int16_t)ADCA.CH1.RES;
-        }
-        if (ch0 < 0)
-            ch0 = 0;
-        if (ch1 < 0)
-            ch1 = 0;
-        g_booster.advals[0] = ch0;
-        g_booster.advals[1] = ch1;
-        ADCA.INTFLAGS = 0x03; // clear intflags
-        ADCA.CTRLA |= Bit(3)|Bit(2); // start conversions
-    }
-}
-
-static uint8_t read_production_signature_row(uint8_t offset) {
-    uint8_t value;
-    NVM.CMD = NVM_CMD_READ_CALIB_ROW_gc;
-    value = pgm_read_byte(offset);
-    NVM.CMD = NVM_CMD_NO_OPERATION_gc;
-    return value;
-}
-#endif // USE_BOOSTER*/
-
 void do_init_system(void) {
     uint8_t portd_bits = 0
                         |Bit(LED_DCC_NOTAUS_b)|Bit(LED_DCC_ON_b)
-/*#ifdef USE_BOOSTER
-                        |7
-#endif*/
+
                         ;
     port_out(PORTD) = portd_bits;
     PORTCFG_MPCMASK = portd_bits;
@@ -1045,19 +761,6 @@ void do_init_system(void) {
 
     PORTCFG_MPCMASK = Bit(NOTAUS_b);
     PORTC.PIN0CTRL = PORT_OPC_PULLUP_gc;
-/*#ifdef USE_BOOSTER
-    port_out(PORTC) = 0;
-    PORTCFG_MPCMASK = 0x0c;
-    PORTC.PIN2CTRL = PORT_OPC_WIREDORPULL_gc;
-    PORTCFG_MPCMASK = 0x43;
-    PORTC.PIN0CTRL = PORT_OPC_TOTEM_gc;
-    port_dirout(PORTC, 0x4f);
-    PORTCFG_MPCMASK = Bit(7);
-    PORTC.PIN7CTRL = PORT_OPC_PULLUP_gc;
-    
-    PORTCFG_MPCMASK = 0x03;
-    PORTA.PIN0CTRL = PORT_OPC_TOTEM_gc;
-#endif*/
     
     // configure sleep mode: idle sleep mode, sleep mode allowed
     SLEEP.CTRL = SLEEP_SMODE_IDLE_gc|Bit(SLEEP_SEN_bp);
@@ -1074,55 +777,12 @@ void do_init_system(void) {
                 |Bit(PR_AC_bp);
     PR.PRPC = Bit(PR_TWI_bp)|Bit(PR_USART1_bp)|Bit(PR_USART0_bp)|Bit(PR_SPI_bp)|Bit(PR_HIRES_bp); // PR_TC1_bp PR_TC0_bp;
     PR.PRPD = Bit(PR_TWI_bp)|Bit(PR_USART1_bp)|Bit(PR_USART0_bp)|Bit(PR_SPI_bp)|Bit(PR_HIRES_bp); // PR_TC1_bp PR_TC0_bp;
-
-/*#ifdef USE_BOOSTER
-    // ADC: use signed mode (unsigned mode may be broken) and Vcc/1.6 reference (1V internal reference may be broken)
-    //      see Atmel Xmega Errata
-    ADCA.CTRLB = Bsv(ADC_CONMODE_bp,1)|Bsv(ADC_FREERUN_bp,0)|ADC_RESOLUTION_12BIT_gc;
-#ifndef __AVR_ATxmega32A4U__
-    // !__AVR_ATxmega32A4U__
-    ADCA.REFCTRL = ADC_REFSEL_VCC_gc; //|Bit(ADC_BANDGAP_bp);
-#else
-    // __AVR_ATxmega32A4U__
-    ADCA.REFCTRL = ADC_REFSEL_INTVCC_gc;
-#endif
-    ADCA.EVCTRL = 0;
-    ADCA.PRESCALER = ADC_PRESCALER_DIV128_gc;
-    ADCA.INTFLAGS = 0x0f;
-    ADCA.CALL = read_production_signature_row(offsetof(NVM_PROD_SIGNATURES_t, ADCACAL0));
-    ADCA.CALH = read_production_signature_row(offsetof(NVM_PROD_SIGNATURES_t, ADCACAL1));
-    ADCA.CTRLA = Bit(ADC_ENABLE_bp);
-    // ADC Channel 0: current
-    ADCA.CH0.CTRL = ADC_CH_GAIN_1X_gc|ADC_CH_INPUTMODE_SINGLEENDED_gc;
-    ADCA.CH0.MUXCTRL = ADC_CH_MUXPOS_PIN0_gc;
-    ADCA.CH0.INTCTRL = ADC_CH_INTMODE_COMPLETE_gc|ADC_CH_INTLVL_OFF_gc;
-    // ADC Channel 1: voltage
-    ADCA.CH1.CTRL = ADC_CH_GAIN_1X_gc|ADC_CH_INPUTMODE_SINGLEENDED_gc;
-    ADCA.CH1.MUXCTRL = ADC_CH_MUXPOS_PIN1_gc;
-    ADCA.CH1.INTCTRL = ADC_CH_INTMODE_COMPLETE_gc|ADC_CH_INTLVL_OFF_gc;
-    // start conversions
-    ADCA.CTRLA |= Bit(3)|Bit(2);
-    
-    // --- AWE ---
-    // AWE nable B and A Channel
-    AWEXC.CTRL = Bit(AWEX_DTICCBEN_bp)|Bit(AWEX_DTICCAEN_bp);
-    AWEXC.DTBOTH = F_CPU_MHZ * 3; // 3us
-    AWEXC.OUTOVEN = 0; // Bit(3)|Bit(2)|Bit(1)|Bit(0);
-    // TC C0 Off
-    TCC0.CTRLA = TC_CLKSEL_OFF_gc;
-    TCC0.CTRLB = TC_WGMODE_FRQ_gc;
-    TCC0.INTCTRLA = 0;
-    TCC0.INTCTRLB = 0;
-#endif*/
     
     g_com.productid = PRODUCT_ID;
     g_com.vendorid = VENDOR_ID;
     g_com.firmware_version = FIRMWARE_VERSION;
     g_com.capabilities = 0
         |CAP_DCC_GENERATOR
-/*#ifdef USE_BOOSTER
-        |CAP_DCC_BOOSTER
-#endif*/
         ;
     g_com.cap_class = 0;
     g_com.dev_desc_P = PSTR(DEVICE_DESC);
@@ -1130,9 +790,7 @@ void do_init_system(void) {
     timer_register(&g_timer_10ms, TIMER_RESOLUTION_1MS);
     
     dccgen_init();
-/*#ifdef USE_BOOSTER
-    booster_init();
-#endif*/
+
 }
 
 uint8_t do_msg(struct sboxnet_msg_header *pmsg) {
@@ -1146,23 +804,13 @@ uint8_t do_msg(struct sboxnet_msg_header *pmsg) {
             if (pmsg->opt.len != 1) {
                 return SBOXNET_ACKRC_INVALID_ARG;
             }
-            uint8_t flags = pmsg->data[0];
+            volatile uint8_t flags = pmsg->data[0];
             if (flags & 0x01) { // on
-                if (
-                        bit_is_set(g_dccgen.flags, DCCGEN_FLAG_NOTAUS_b) ||
-
-/*#ifdef USE_BOOSTER
-                        bit_is_set(g_booster.flags, BOOSTER_FLAG_NOTAUS_b) ||
-#endif*/
-                        bit_is_set(g_dev_state, DEV_STATE_FLG_WATCHDOG_b)) {
+                if (bit_is_set(g_dccgen.flags, DCCGEN_FLAG_NOTAUS_b)
+                        /*||
+                        bit_is_set(g_dev_state, DEV_STATE_FLG_WATCHDOG_b)*/) {
                     return SBOXNET_ACKRC_LOCO_NOTAUS;
                 }
-/*#ifdef USE_BOOSTER
-                if (bit_is_clear(g_booster.flags, BOOSTER_FLAG_ON_b)) {
-                    booster_power_on_track();
-                }
-                setbit(g_booster.flags, BOOSTER_FLAG_ON_b);
-#endif*/
                 if (bit_is_clear(g_dccgen.flags, DCCGEN_FLAG_ON_b) && g_dcc.state == DCC_STATE_NONE) {
                     if (flags & 0x02) {
                         loco_list_reset();
@@ -1173,11 +821,6 @@ uint8_t do_msg(struct sboxnet_msg_header *pmsg) {
                 }
                 setbit(g_dccgen.flags, DCCGEN_FLAG_ON_b);
             } else { // off
-/*#ifdef USE_BOOSTER
-                booster_power_off_all();
-                clrbit(g_booster.flags, BOOSTER_FLAG_ON_b);
-                clrbit(g_booster.flags, BOOSTER_FLAG_NOTAUS_b);
-#endif*/
                 dcc_stop_generator(&g_dcc);
                 g_locos.pcur = 0;
                 if (!(flags & 0x02)) {
@@ -1258,34 +901,12 @@ uint8_t do_reg_read(uint16_t reg, uint16_t* pdata) {
         case R_DCCGEN_NUM_LOCOS: *pdata = g_locos.numlocos; return 0;
         case R_DCCGEN_LOCOADDR_SCAN_MAX: *pdata = g_dccgen.locoaddr_scan_max; return 0;
         case R_DCCGEN_LOCOADDR_SCAN_CUR: *pdata = g_dccgen.locoaddr_scan_cur; return 0;
-/*#ifdef USE_BOOSTER
-        case R_BOOSTER_FLAGS: *pdata = g_booster.flags; return 0;
-        case R_ADCVAL_NUM: *pdata = 2; return 0;
-        case R_ADCVAL_0: *pdata = g_booster.advals[0]; return 0;
-        case R_ADCVAL_1: *pdata = g_booster.advals[1]; return 0;
-        
-        case R_BOOSTER_SHORTCUT_LIMIT:  *pdata = g_booster.shortcut_limit; return 0;
-        case R_BOOSTER_SHORTCUT_CNT:    *pdata = g_booster.shortcut_nummax; g_booster.shortcut_nummax = 0; return 0;
-        case R_BOOSTER_SHORTCUT_INTERVAL: *pdata = g_booster.shortcut_interval; return 0;
-#endif*/
     }    
     return SBOXNET_ACKRC_REG_INVALID;
 };
 
 uint8_t do_reg_write(uint16_t reg, uint16_t data, uint16_t mask) {
     switch (reg) {
-/*#ifdef USE_BOOSTER
-        case R_BOOSTER_SHORTCUT_LIMIT: {
-            g_booster.shortcut_limit = data;
-            g_booster.eeprom_flags.write_shortcut_limit = 1;
-            return 0;
-        }
-        case R_BOOSTER_SHORTCUT_INTERVAL: {
-            g_booster.shortcut_interval = data;
-            g_booster.eeprom_flags.write_shortcut_interval = 1;
-            return 0;
-        }
-#endif*/
         case R_DCCGEN_LOCOADDR_SCAN_MAX: {
             g_dccgen.locoaddr_scan_max = data;
             g_dccgen.eeprom_flags.write_locoaddr_scan_max = 1;
@@ -1314,28 +935,13 @@ void do_main(void) {
         loco_timing();
         dcc_read_switches();
     }
-/*#ifdef USE_BOOSTER
-    if (timer_timedout(&g_booster.timer_startup)) {
-        timer_set(&g_booster.timer_startup, 0xff);
-        if (PORTC.INTCTRL & PORT_INT1LVL_gm) {
-            booster_sensors_shortcut_on();
-        }
-    }
-
-    if (bit_is_set(g_dev_state, DEV_STATE_FLG_WATCHDOG_b) || (bit_is_set(g_booster.flags, BOOSTER_FLAG_ON_b) && timer_timedout(&g_booster.timer_dcc_watchdog))) {
-        booster_notaus();
-        clrbit(g_booster.flags, BOOSTER_FLAG_ON_b);
-    }
-    
-    led_set(LED_BO_NOTAUS_b, bit_is_set(g_booster.flags, BOOSTER_FLAG_NOTAUS_b));
-#endif*/
     
     loco_task();
     
     if (bit_is_set(g_dccgen.switches, SWITCH_NOTAUS_b)) {
         setbit(g_dccgen.flags, DCCGEN_FLAG_NOTAUS_b);
     }
-    if (bit_is_set(g_dev_state, DEV_STATE_FLG_WATCHDOG_b) || bit_is_set(g_dccgen.flags, DCCGEN_FLAG_NOTAUS_b)) {
+    if (/*bit_is_set(g_dev_state, DEV_STATE_FLG_WATCHDOG_b) ||*/ bit_is_set(g_dccgen.flags, DCCGEN_FLAG_NOTAUS_b)) {
         if (bit_is_set(g_dccgen.flags, DCCGEN_FLAG_ON_b)) {
             clrbit(g_dccgen.flags, DCCGEN_FLAG_ON_b);
             dcc_stop_generator(&g_dcc);
@@ -1345,17 +951,6 @@ void do_main(void) {
     led_set(LED_DCC_ON_b,     bit_is_set(g_dccgen.flags, DCCGEN_FLAG_ON_b));
     led_set(LED_DCC_NOTAUS_b, bit_is_set(g_dccgen.flags, DCCGEN_FLAG_NOTAUS_b));
 
-/*#ifdef USE_BOOSTER
-    measure_task();
-    if (g_booster.eeprom_flags.write_shortcut_limit && eeprom_is_ready()) {
-        eeprom_update_word(&g_eeprom.booster.shortcut_limit, g_booster.shortcut_limit);
-        g_booster.eeprom_flags.write_shortcut_limit = 0;
-    }
-    if (g_booster.eeprom_flags.write_shortcut_interval && eeprom_is_ready()) {
-        eeprom_update_word(&g_eeprom.booster.shortcut_interval, g_booster.shortcut_interval);
-        g_booster.eeprom_flags.write_shortcut_interval = 0;
-    }
-#endif*/
     if (g_dccgen.eeprom_flags.write_locoaddr_scan_max && eeprom_is_ready()) {
         eeprom_update_word(&g_eeprom.dccgen.locoaddr_scan_max, g_dccgen.locoaddr_scan_max);
         g_dccgen.eeprom_flags.write_locoaddr_scan_max = 0;
@@ -1365,9 +960,6 @@ void do_main(void) {
 }
 
 void do_before_bldr_activate(void) {
-/*#ifdef USE_BOOSTER
-    booster_power_off_all();
-#endif*/
 
     dcc_stop_generator(&g_dcc);
     
